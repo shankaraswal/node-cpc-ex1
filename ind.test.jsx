b8aa661f02ec4cda33b948f5ec4583537974bd33
Shankar Aswal <shankaraswal@gmail.com>
2024-03-28 21:37:21 +0000

    test ind.test.tsx1

--- ind.test.jsx
+++ ind.test.jsx
@@ -1,30 +1,31 @@
-import { describe, it } from "mocha";
-import { expect } from "chai";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Ind from "../ind";
 
-import { commonUtils } from "./ind";
-
-describe("commonUtils", () => {
-  describe("isString", () => {
-    it("should return true if value is a string", () => {
-      const result = commonUtils.isString("hello");
-      expect(result).to.be.true;
-    });
-
-    it("should return false if value is not a string", () => {
-      const result = commonUtils.isString(123);
-      expect(result).to.be.false;
-    });
+describe("Ind", () => {
+  test("renders without crashing", () => {
+    render(<Ind />);
+    expect(screen.getByTestId("ind")).toBeInTheDocument();
   });
 
-  describe("addNumbers", () => {
-    it("should add two numbers", () => {
-      const result = commonUtils.addNumbers(1, 2);
-      expect(result).to.equal(3);
-    });
+  test("increments count when button clicked", () => {
+    render(<Ind />);
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    expect(screen.getByTestId("count")).toHaveTextContent("1");
+  });
 
-    it("should return 0 if no arguments", () => {
-      const result = commonUtils.addNumbers();
-      expect(result).to.equal(0);
-    });
+  test("decrements count when button clicked", () => {
+    render(<Ind count={1} />);
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    expect(screen.getByTestId("count")).toHaveTextContent("0");
+  });
+
+  test("count does not go below 0", () => {
+    render(<Ind count={0} />);
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    expect(screen.getByTestId("count")).toHaveTextContent("0");
   });
 });
